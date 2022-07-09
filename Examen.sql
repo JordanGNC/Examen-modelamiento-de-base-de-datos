cd6e280325f377292a09964dd3a5407b72646f00
JordanGNC <jord.navarro@duocuc.cl>
2022-07-09 07:51:03 +0000

    Nuevs tablas con PK y FK / Nuevvos INSERT INTO

--- Examen.sql
+++ Examen.sql
@@ -1,6 +1,6 @@
---Creacion de DATE
+--Creacion de DATE (Crear Date)
 
-ALTER SESSION SET NLS_DATE_FORMAT= 'DD/MM/YYYY'
+ALTER SESSION SET NLS_DATE_FORMAT= 'DD/MM/YYYY';
 
 --Creacion de tablas
 
@@ -74,8 +74,9 @@ CREATE TABLE pagos (
     valor_a_pagar               NUMBER(20) NOT NULL,
     bancos_id                   NUMBER NOT NULL,
     cheques_id                  NUMBER NULL,
-    formas_de_pago_id           NUMBER NOT NULL,
-    afiliados_pagos_id          NUMBER NOT NULL
+    formas_de_pago_id           NUMBER NULL,
+    afiliados_pagos_id          NUMBER NOT NULL,
+    tipopago_pagos_id           NUMBER NULL
 );
 
 CREATE TABLE formas_de_pagos (
@@ -111,14 +112,35 @@ CREATE TABLE regiones (
 );
 
 CREATE TABLE vehiculo (
-    id_vehiculo                     NUMBER NOT NULL,
-    marca                       VARCHAR2(250) NOT NULL,
-    modelo_vehiculo             VARCHAR2(250) NOT NULL,
+    id_vehiculo                 NUMBER NOT NULL,
     patente                     VARCHAR2(20) NOT NULL,
-    color                       VARCHAR2(250) NOT NULL,
     numero_chasis               NUMBER(20) NOT NULL,
     numero_motor                NUMBER(20) NOT NULL,
-    afiliado_vehiculo_id        NUMBER NOT NULL
+    afiliado_vehiculo_id        NUMBER NOT NULL,
+    modelosveh_vehiculo_id      NUMBER NOT NULL,
+    coloresveh_vehiculos_id     NUMBER NOT NULL,
+    tiposveh_vehiculos_id       NUMBER NOT NULL
+);
+
+CREATE TABLE colores_de_vehiculos (
+    id_color                    NUMBER NOT NULL,
+    color_vehiculo              VARCHAR2(250) NOT NULL
+);
+
+CREATE TABLE marcas_de_vehiculos (
+    id_marca                    NUMBER NOT NULL,
+    marca_vehiculo              VARCHAR(250) NOT NULL
+);
+
+CREATE TABLE modelos_de_vehiculos (
+    id_modelo                   NUMBER NOT NULL,
+    modelo_vehiculo             VARCHAR2(250) NOT NULL,
+    marca_modelo_id             NUMBER NOT NULL
+);
+
+CREATE TABLE tipos_de_vehiculos (
+    id_tipovehiculo             NUMBER NOT NULL,
+    tipo_vehiculo               VARCHAR2(250) NOT NULL
 );
 
 CREATE TABLE estado_de_vehiculo (
@@ -165,11 +187,11 @@ CREATE TABLE beneficios (
     sindicato_beneficios_id     NUMBER NOT NULL
 );
 
-CREATE TABLE discapacidad (
+CREATE TABLE discapacidad_afiliado (
     id_discapacidad             NUMBER NOT NULL,
-    cuenta_con_discapacidad     VARCHAR(250) NOT NULL,
+    discapacitado               VARCHAR(250) NOT NULL,
     afiliados_id                NUMBER NOT NULL,
-    tipo_de_discapacidad_id     NUMBER NOT NULL
+    tipo_de_discapacidad_id     NUMBER NULL
 );
 
 CREATE TABLE tipo_de_discapacidad (
@@ -177,8 +199,18 @@ CREATE TABLE tipo_de_discapacidad (
     nombre_de_discapacidad      VARCHAR2(250) NOT NULL
 );
 
+CREATE TABLE tipos_de_pago (
+    id_tipopago                 NUMBER NOT NULL,
+    tipo_pago                   VARCHAR2(250) NOT NULL
+);
+
 --Creacion de PK
 
+ALTER TABLE tipos_de_pago ADD CONSTRAINT tipos_de_pago_PK PRIMARY KEY (id_tipopago);
+ALTER TABLE marcas_de_vehiculos ADD CONSTRAINT marcas_de_vehiculos_PK PRIMARY KEY (id_marca);
+ALTER TABLE modelos_de_vehiculos ADD CONSTRAINT modelos_de_vehiculos_PK PRIMARY KEY (id_modelo);
+ALTER TABLE colores_de_vehiculos ADD CONSTRAINT colores_de_vehiculos_PK PRIMARY KEY (id_color);
+ALTER TABLE tipos_de_vehiculos ADD CONSTRAINT tipos_de_vehiculos_PK PRIMARY KEY (id_tipovehiculo);
 ALTER TABLE afiliados ADD CONSTRAINT afiliados_PK PRIMARY KEY (id_afiliados);
 ALTER TABLE nacionalidad ADD CONSTRAINT nacionalidad_PK PRIMARY KEY (id_nacionalidad);
 ALTER TABLE carga ADD CONSTRAINT carga_PK PRIMARY KEY (id_carga);
@@ -201,7 +233,7 @@ ALTER TABLE empresa ADD CONSTRAINT empresa_PK PRIMARY KEY (id_empresa);
 ALTER TABLE sindicato ADD CONSTRAINT sindicato_PK PRIMARY KEY (id_sindicato);
 ALTER TABLE beneficio_transporte ADD CONSTRAINT beneficio_transporte PRIMARY KEY (id_beneficiotransporte);
 ALTER TABLE beneficios ADD CONSTRAINT beneficios_PK PRIMARY KEY (id_beneficio);
-ALTER TABLE discapacidad ADD CONSTRAINT discapacidad_PK PRIMARY KEY (id_discapacidad);
+ALTER TABLE discapacidad_afiliado ADD CONSTRAINT discapacidad_afiliado_PK PRIMARY KEY (id_discapacidad);
 ALTER TABLE tipo_de_discapacidad ADD CONSTRAINT tipo_de_discapacidad_PK PRIMARY KEY (id_tipodediscapacidad);
 
 --Creacion de FK
@@ -216,9 +248,9 @@ ALTER TABLE afiliados ADD CONSTRAINT afiliados_solicitud_FK FOREIGN KEY (estado_
     REFERENCES estado_de_solicitud (id_solicitud);
 ALTER TABLE afiliados ADD CONSTRAINT afiliados_nacionalidad_FK FOREIGN KEY (nacionalidad_id)
     REFERENCES nacionalidad (id_nacionalidad);
-ALTER TABLE discapacidad ADD CONSTRAINT discapacidad_afiliado_FK FOREIGN KEY (afiliados_id)
+ALTER TABLE discapacidad_afiliado ADD CONSTRAINT discapacidad_afiliado_FK FOREIGN KEY (afiliados_id)
     REFERENCES afiliados (id_afiliados);
-ALTER TABLE discapacidad ADD CONSTRAINT discapacidad_tipodiscapacidad_FK FOREIGN KEY (tipo_de_discapacidad_id)
+ALTER TABLE discapacidad_afiliado ADD CONSTRAINT discapacidad_tipodiscapacidad_FK FOREIGN KEY (tipo_de_discapacidad_id)
     REFERENCES tipo_de_discapacidad (id_tipodediscapacidad);
 ALTER TABLE telefonos ADD CONSTRAINT telefonos_afiliados FOREIGN KEY (afiliados_telefonos_id)
     REFERENCES afiliados (id_afiliados);
@@ -256,32 +288,68 @@ ALTER TABLE vehiculo ADD CONSTRAINT vehiculo_afiliado_FK FOREIGN KEY (afiliado_v
     REFERENCES afiliados (id_afiliados);
 ALTER TABLE beneficio_transporte ADD CONSTRAINT beneficiotrans_sindicato_FK FOREIGN KEY (sindicato_beneficiotrans_id)
     REFERENCES sindicato (id_sindicato);
+ALTER TABLE modelos_de_vehiculos ADD CONSTRAINT modelo_marca_FK FOREIGN KEY (marca_modelo_id)
+    REFERENCES marcas_de_vehiculos (id_marca);
+ALTER TABLE vehiculo ADD CONSTRAINT vehiculo_modelo_FK FOREIGN KEY (modelosveh_vehiculo_id)
+    REFERENCES modelos_de_vehiculos (id_modelo);
+ALTER TABLE vehiculo ADD CONSTRAINT vehiculo_color_FK FOREIGN KEY (coloresveh_vehiculos_id)
+    REFERENCES colores_de_vehiculos (id_color);
+ALTER TABLE vehiculo ADD CONSTRAINT vehiculo_tipoveh_FK FOREIGN KEY (tiposveh_vehiculos_id)
+    REFERENCES tipos_de_vehiculos (id_tipovehiculo);
+ALTER TABLE pagos ADD CONSTRAINT pagos_tipopago_FK FOREIGN KEY (tipopago_pagos_id)
+    REFERENCES tipos_de_pago (id_tipopago);
 
---Creacion de INSERT
+--Creacion de INSERT (Crear uno por uno de manera vertical descendiente)
 
+INSERT INTO marcas_de_vehiculos VALUES (1,'Chevrolet');
+INSERT INTO marcas_de_vehiculos VALUES (2,'Suzuki');
+INSERT INTO modelos_de_vehiculos VALUES (1,'Camaro',1);
+INSERT INTO modelos_de_vehiculos VALUES (2,'Spresso',2);
+INSERT INTO colores_de_vehiculos VALUES (1,'Amarillo');
+INSERT INTO colores_de_vehiculos VALUES (2,'Naranja');
+INSERT INTO tipos_de_vehiculos VALUES (1,'Deportivo');
+INSERT INTO tipos_de_vehiculos VALUES (2,'Camioneta');
+INSERT INTO tipos_de_pago VALUES (1,'Cuotas');
 INSERT INTO estado_civil VALUES (1,'Casado');
 INSERT INTO parentesco VALUES (1,'Hijo');
+INSERT INTO parentesco VALUES (2,'Conyuge');
 INSERT INTO nacionalidad VALUES (1,'Chile');
-INSERT INTO estado_de_solicitud VALUES (1,'22/01/2022','ACEPTADA');
+INSERT INTO nacionalidad VALUES (2,'Venezuela');
+INSERT INTO estado_de_solicitud VALUES (1,'22/01/2022','ACEPTADA'); 
 INSERT INTO regiones VALUES (1,'Region Metropolitana');
-INSERT INTO provincias VALUES (1,'Santiago',1);
+INSERT INTO regiones VALUES (2,'Maule');
+INSERT INTO provincias VALUES (1,'Santiago',1); 
+INSERT INTO provincias VALUES (2,'Talca',2);
 INSERT INTO comunas VALUES (1,'San joaquin',1);
+INSERT INTO comunas VALUES (2,'Rio claro',2);
 INSERT INTO afiliados VALUES (1,'Jordan','','Navarro','','',20940071,'5','jord.navarro@duocuc.cl','22/12/2001','central oriente 123',1,1,1);
-INSERT INTO carga VALUES (1,'Jordancito','','Navarro','',22940071,'k','1/1/2005',1,'',1);
+INSERT INTO afiliados VALUES (2,'Jesus','Cristo','Delgado','','64826404','','','Yisus@duocuc.cl','01/01/2004','Su casita 123',2,1,2);
+INSERT INTO carga VALUES (1,'JordanJr','Twentytree','Navarro','',22940071,'k','01/01/2018',1,'',1);
+INSERT INTO carga VALUES (2,'Juanita','','Delgado','',12345678,'0','01/01/2003',2,1,2);
 INSERT INTO tipos_de_telefonos VALUES (1,'Celular');
+INSERT INTO tipos_de_telefonos VALUES (2,'Fijo');
 INSERT INTO telefonos VALUES (1,'35378516',1,1);
+INSERT INTO telefonos VALUES (2,'52345629',2,2);
 INSERT INTO formas_de_pagos VALUES (1,'Transferencia');
 INSERT INTO cheques VALUES (1,'123456789');
 INSERT INTO bancos VALUES (1,'Banco Estado');
-INSERT INTO vehiculo VALUES (1,'Chevrolet','Camaro ss','HDR1234','Amarillo','123456789','987654321',1);
+INSERT INTO bancos VALUES (2,'Banco de chile');
+INSERT INTO vehiculo VALUES (1,'HDR1234','123456789','987654321',1,1,1,1);
+INSERT INTO vehiculo VALUES (2,'AWD8596','276527435','987324984',2,2,2,2);
 INSERT INTO empresa VALUES (1,'SINTAC','Yal 123',1);
 INSERT INTO aseguradora_de_vehiculo VALUES (1,'TU VEHICULO EN BUENAS MANOS','Estacion central 123',1);
 INSERT INTO sindicato VALUES (1,'SYAC',1);
 INSERT INTO beneficio_transporte VALUES (1,'Terrestre','30%',1);
+INSERT INTO beneficio_transporte VALUES (2,'Maritimo','23,25%',1);
 INSERT INTO beneficios VALUES (1,'15%','Vida del deportista','4',1,1);
-INSERT INTO tipo_de_discapacidad VALUES (1,'Epilepsia');
-INSERT INTO discapacidad VALUES (1,'No',1,1);
+INSERT INTO beneficios VALUES (2,'50%','Clinica dental Santa Maria','1',2,1);
+INSERT INTO tipo_de_discapacidad VALUES (1,'Esquizofrenia');
+INSERT INTO discapacidad_afiliado VALUES (1,'No',1,'');
+INSERT INTO discapacidad_afiliado VALUES (2,'Si',2,1);
+INSERT INTO pagos VALUES (1,'09/07/2022','6.000',1,1,'',1,1);
+INSERT INTO pagos VALUES (2,'09/06/2022','100.000',2,'',1,2,1);
 
 --Creacion de SELECT
 
 SELECT * FROM afiliados;
+SELECT * FROM carga;
